166813989ea73d466c890ba809259f65731237e9
Baidi Wang <baidiwang30@gmail.com>
2022-07-10 15:16:40 -0400

    final code

--- src/store.js
+++ src/store.js
@@ -1,25 +1,51 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
 
 const initialState = {
   groceries: [],
   view: ''
 };
-const store = createStore((state = initialState, action)=> {
-  if(action.type === 'LOAD'){
-    state = {...state, groceries: action.groceries };
-  }
-  if(action.type === 'UPDATE'){
-    state = {...state, groceries: state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )};
-  }
-  if(action.type === 'CREATE'){
-    state = {...state, groceries: [...state.groceries, action.grocery ]}
-  }
+
+const viewReducer = (state = window.location.hash.slice(1), action) => {
   if(action.type === 'SET_VIEW'){
-    state = {...state, view: action.view};
+   return action.view;
   }
   return state;
+}
+
+const groceryReducer = (state = [], action) => {
+  // if(action.type === 'LOAD'){
+  //   return [...state, action.createGroceries];
+  // }
+  if(action.type === 'UPDATE'){
+    return state.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery );
+  }
+  if(action.type === 'CREATE'){
+    state = [...state, action.grocery ]
+  }
+  return state;
+ }
+
+
+const reducer = combineReducers({
+  groceries: groceryReducer,
+  view: viewReducer
 });
 
+
+const createGroceries = () => {
+  return async(dispatch) =>{
+    const grocery = (await axios.post('/api/groceries/random')).data;
+    dispatch({ type: 'CREATE', grocery });
+  }
+}
+
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 export default store;
+
+export { createGroceries };
 
 
